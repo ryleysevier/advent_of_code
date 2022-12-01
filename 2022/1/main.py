aac3e9f25c2535dd6e347ed8c18362f9157d687d
ryleysevier <ryleysevier@gmail.com>
2022-12-01 10:41:06 -0500

    + Day 1, Part 2

--- 2022/1/main.py
+++ 2022/1/main.py
@@ -12,16 +12,30 @@ def p1(data):
     return(largest, o[largest])
 
 
-data = []
-with open('sample_input.txt') as file:
-    data = file.readlines()
-    data = [line.rstrip() for line in data]
+def p2(data):
+    o = [0]
+    i = 0
+    for d in data:
+        if d == '': 
+            o.append(0)
+            i+=1 
+            continue
+        o[i]+= int(d)
+    o.sort()
+    return(sum(o[-3:]))
 
-print(p1(data))
+
+sample = []
+with open('sample_input.txt') as file:
+    sample = file.readlines()
+    sample = [line.rstrip() for line in sample]
 
 data = []
 with open('input_1.txt') as file:
     data = file.readlines()
     data = [line.rstrip() for line in data]
 
+print(p1(sample))
 print(p1(data))
+print(p2(sample))
+print(p2(data))
