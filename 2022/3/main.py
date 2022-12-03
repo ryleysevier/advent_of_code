1cb879bd5968cdd083b30ca04e336f7f1de647b1
ryleysevier <ryleysevier@gmail.com>
2022-12-03 12:04:24 -0500

    +Day 3, Part 2

--- 2022/3/main.py
+++ 2022/3/main.py
@@ -1,12 +1,29 @@
+def score(o):
+    o = ord(o) 
+    if o > 97: return o - 96
+    return o - 64 + 26
+
 def p1(data):
     p = 0
     for d in data:
         l = d[:int(len(d)/2)]
         r = d[-int(len(d)/2):]
         b = set(l).intersection(set(r))
-        o = ord(list(b)[0]) 
-        if o > 97: p += o - 96
-        else: p += o - 64 + 26
+        p += score(ord(list(b)[0]))
+        
+    return p
+
+def p2(data):
+    p = 0
+    for r in range(0, int(len(data)/3)):
+        i = r*3
+        a = data[i]
+        b = data[i+1]
+        c = data[i+2]
+
+        x = set(a).intersection(set(b))
+        y = x.intersection(set(c))
+        p += score(ord(list(b)[0]))
         
     return p
 
@@ -23,3 +40,5 @@ with open('./2022/3/input_1.txt') as file:
 
 print(p1(sample))
 print(p1(data))
+print(p2(sample))
+print(p2(data))
